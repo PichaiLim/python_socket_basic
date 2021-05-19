8903abe6b0ab6afcc1a993163b0359b68b0138e3
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2021-05-19 14:08:51 +0700

    add deray time

--- multitask.py
+++ multitask.py
@@ -6,15 +6,18 @@ import threading
 def Toothbrush():
     for i in range(10):
         print(f"{i} แปรฟังอยู่โว้ย......")
+        time.sleep(0.3)
 
 def Shower():
     for i in range(10):
         print(f"{i} กำลังอาบน้ำ...")
+        time.sleep(1)
 
 start = time.time() # จำเวลาเริ่มต้น
 
+''' เรียกใช้งาน function '''
 Toothbrush()
 Shower()
 
-end = time.time()
+end = time.time() # จำเวลาสิ้นสุด
 print('All Time: ', end - start)
