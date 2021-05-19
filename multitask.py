f9e8fe04fddb3256808fd7eee6cc8cffacc50cc5
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2021-05-19 13:57:41 +0700

    สร้าง function 2 ตัวเพื่อแสดงผล

--- multitask.py
+++ multitask.py
@@ -1,1 +1,15 @@
 # multitask.py
+
+import time
+import threading
+
+def Toothbrush():
+    for i in range(10):
+        print(f"{i} แปรฟังอยู่โว้ย......")
+
+def Shower():
+    for i in range(10):
+        print(f"{i} กำลังอาบน้ำ...")
+
+Toothbrush()
+Shower()
