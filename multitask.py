ef7afcfd97f0795e0e1d443ee209566776abd7c4
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2021-05-19 14:32:09 +0700

    success threading

--- multitask.py
+++ multitask.py
@@ -5,18 +5,18 @@ import threading
 
 def Toothbrush(brand):
     for i in range(10):
-        print(f"{i} แปรฟังอยู่โว้ย......{brand}")
+        print(f"{i} แปรฟังอยู่โว้ย......ยาสีฟันยี่ห้อ: {brand}")
         time.sleep(0.3)
 
 def Shower(soap, gel):
     for i in range(10):
-        print(f"{i} กำลังอาบน้ำ...")
+        print(f"{i} กำลังอาบน้ำ...สบู่: {soap} | ยาสระผม: {gel}")
         time.sleep(1)
 
 
 ''' TODO Thread '''
-task1 = threading.Thread(target=Toothbrush, args=('Hello',))
-task2 = threading.Thread(target=Shower)
+task1 = threading.Thread(target=Toothbrush, args=('Hello',)) # กรณีที่ส่งค่า 1 ตัว ให้เดิมเครื่องหมาย ',' เพื่อให้ไม่ให้ Error
+task2 = threading.Thread(target=Shower, args=('สบู่ไทย','เจลกุหลาบ'))
 
 
 
